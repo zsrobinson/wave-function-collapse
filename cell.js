53e67ec7307fc7cbd7563fd9f81854eb295bbc82
Zachary Robinson <zsrobinson@proton.me>
2022-07-12 21:38:55 -0400

    first commit

---
+++ cell.js
@@ -0,0 +1,47 @@
+class Cell {
+    constructor(i, j) {
+        this.tile = null;
+        this.i = i;
+        this.j = j;
+    }
+
+    draw() {
+        let x = this.i * width / cellNum;
+        let y = this.j * height / cellNum;
+        let w = width / cellNum;
+        let h = height / cellNum;
+        strokeWeight(0);
+
+        if (this.tile) {
+            image(this.tile.img, x, y, w, h);
+            console.log("drawing tile " + this.tile.sides)
+        } else {
+            fill(64);
+            rect(x, y, w, h);
+        }
+    }
+
+    determineEntropy() {
+        let possibleTiles = tiles.slice(); // creates a copy of the array
+
+        // check up
+        if (this.i - 1 >= 0) { // if it exists
+            //console.log("the cell above me exists")
+            if (cells[this.i][this.j-1].tile) { // and if it has a tile
+                //console.log("the cell above me has a tile")
+                for (var k = possibleTiles.length -1; k >= 0 ; k--) {
+                    if (cells[this.i][this.j-1].tile.sides[2] != possibleTiles[k].sides[0]) { // if the sides don't match
+                        //console.log("the cell above me has a tile that doesn't match")
+                        possibleTiles.splice(k, 1); // remove it
+                    }
+                }
+            }
+        }
+
+        // set this.tile to a random tile from the possibleTiles array
+        this.tile = possibleTiles[Math.floor(Math.random() * possibleTiles.length)];
+        this.draw()
+
+        return possibleTiles;
+    }
+}
---
+++ script.js
@@ -0,0 +1,38 @@
+const cellNum = 4;
+let tiles = [];
+let cells = [];
+
+function setup() {
+    createCanvas(512, 512);
+
+    tiles.push(new Tile([1, 1, 0, 1], loadImage("img/tee.png")));
+    tiles.push(new Tile([1, 1, 0, 0], loadImage("img/bend.png")));
+    tiles.push(new Tile([1, 1, 1, 1], loadImage("img/cross.png")));
+    tiles.push(new Tile([0, 0, 0, 0], loadImage("img/blank.png")));
+    
+    for (let i = 0; i < 2; i++) {
+        for (let r = 0; r < 3; r++) {
+            tiles.push(tiles[i].rotate(r));
+        }
+    }   
+
+    for (let i = 0; i < cellNum; i++) {
+        cells.push([]);
+        for (let j = 0; j < cellNum; j++) {
+            cells[i][j] = new Cell(i, j);
+        }
+    }
+
+    cells[1][1].tile = tiles[3];
+    
+}
+
+function draw() {
+    for (let i = 0; i < cellNum; i++) {
+        for (let j = 0; j < cellNum; j++) {
+            cells[i][j].draw();
+        }
+    }
+    console.log(cells[1][2].determineEntropy())
+    noLoop()
+}
