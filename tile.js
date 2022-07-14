fdc7a590c344ccdd13a1431cf9ceb8aacd14ef43
Zachary Robinson <zsrobinson@proton.me>
2022-07-14 15:40:06 -0400

    Convert indentation to tabs

--- tile.js
+++ tile.js
@@ -1,27 +1,27 @@
 class Tile {
-    constructor(sides, img) {
-        this.sides = sides;
-        this.img = img;
-    }
+	constructor(sides, img) {
+		this.sides = sides;
+		this.img = img;
+	}
 
-    rotate(n) { // pretty much sourced directly from The Coding Train
-        
-        // rotate the image
-        const w = this.img.width;
-        const h = this.img.height;
-        const newImg = createGraphics(w, h);
-        newImg.imageMode(CENTER);
-        newImg.translate(w / 2, h / 2);
-        newImg.rotate(HALF_PI * n);
-        newImg.image(this.img, 0, 0);
+	rotate(n) { // pretty much sourced directly from The Coding Train
+		
+		// rotate the image
+		const w = this.img.width;
+		const h = this.img.height;
+		const newImg = createGraphics(w, h);
+		newImg.imageMode(CENTER);
+		newImg.translate(w / 2, h / 2);
+		newImg.rotate(HALF_PI * n);
+		newImg.image(this.img, 0, 0);
 
-        // rotate the sides array
-        const newSides = [];
-        const len = this.sides.length;
-        for (let i = 0; i < len; i++) {
-            newSides[i] = this.sides[(i - n + len) % len];
-        }
+		// rotate the sides array
+		const newSides = [];
+		const len = this.sides.length;
+		for (let i = 0; i < len; i++) {
+			newSides[i] = this.sides[(i - n + len) % len];
+		}
 
-        return new Tile(newSides, newImg);
-    }
+		return new Tile(newSides, newImg);
+	}
 }
